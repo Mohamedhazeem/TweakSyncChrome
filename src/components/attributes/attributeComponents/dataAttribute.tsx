536d144090bb5f4d6322549b98c85d87a4e3b322
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-06-27 21:22:22 +0530

    data input sample code

--- src/components/attributes/attributeComponents/dataAttribute.tsx
+++ src/components/attributes/attributeComponents/dataAttribute.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { useAttributeContext } from "@/utils/attributesContext";
+import { ChangeEvent, useState, KeyboardEvent } from "react";
 
 function DataAttribute() {
   const context = useAttributeContext();
@@ -41,3 +42,57 @@ function DataAttribute() {
 }
 
 export default DataAttribute;
+
+interface DataInputProps {
+  initialKey?: string;
+  initialValue?: string;
+  onKeyChange: (newKey: string) => void;
+  onValueChange: (newValue: string) => void;
+}
+
+export const DataInput: React.FC<DataInputProps> = ({
+  initialKey = "data-",
+  initialValue = "",
+  onKeyChange,
+  onValueChange,
+}) => {
+  const prefix = "data-";
+  const [key, setKey] = useState<string>(initialKey);
+  const [value, setValue] = useState<string>(initialValue);
+
+  const handleKeyChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newKey = e.target.value;
+    // Ensure the prefix is persistent
+    if (newKey.startsWith(prefix)) {
+      setKey(newKey);
+      // Pass only the user-defined part (after the prefix) to the parent
+      onKeyChange(newKey.slice(prefix.length));
+    } else {
+      setKey(prefix);
+    }
+  };
+
+  const handleValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    onValueChange(newValue);
+  };
+
+  const handleKeyKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Backspace" && key === prefix) {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={key}
+        onChange={handleKeyChange}
+        onKeyDown={handleKeyKeyDown}
+      />
+      <input type="text" value={value} onChange={handleValueChange} />
+    </div>
+  );
+};
