2cfe30e0d6e391996c1f79ef91e65d4fa4d3f1f1
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-06-28 11:17:14 +0530

    class input removal logic

--- src/components/attributes/attributeComponents/classAttribute.tsx
+++ src/components/attributes/attributeComponents/classAttribute.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 // import { IAttributeContext } from "@/types/attributeTypes";
 import { useAttributeContext } from "@/utils/attributesContext";
+import { useEffect, useState } from "react";
 function ClassAttribute() {
   const context = useAttributeContext();
+  const [words, setWords] = useState<string[]>([]);
+  useEffect(() => {
+    if (context?.attribute?.value) {
+      setWords(splitStringToArray(context.attribute.value.toString()));
+    }
+  }, [context?.attribute]);
   if (!context?.attribute) {
     return null;
   }
@@ -11,28 +19,38 @@ function ClassAttribute() {
     return words;
   }
   const handleInputChange = (wordIndex: number, newValue: string) => {
-    if (context?.attribute) {
-      const words = splitStringToArray(context.attribute.value.toString());
-      words[wordIndex] = newValue;
-      const updatedValue = words.join(" ");
-      context.onChange(context.index!, updatedValue);
-    }
+    const updatedWords = [...words];
+    updatedWords[wordIndex] = newValue;
+    setWords(updatedWords);
+    const updatedValue = updatedWords.join(" ");
+    context.onChange(context.index!, updatedValue);
+  };
+
+  const handleRemoveClick = (wordIndex: number) => {
+    const updatedWords = [...words];
+    updatedWords.splice(wordIndex, 1);
+    setWords(updatedWords);
+    const updatedValue = updatedWords.join(" ");
+    context.onChange(context.index!, updatedValue);
   };
   return (
     <div key={context?.key}>
-      {typeof context?.attribute?.type === "boolean" ||
-      context?.attribute?.type === "string"
-        ? splitStringToArray(context?.attribute?.value.toString()).map(
-            (word, wordIndex) => (
-              <Input
-                key={wordIndex}
-                type="text"
-                value={word}
-                onChange={(e) => handleInputChange(wordIndex, e.target.value)}
-              />
-            )
-          )
-        : String(context?.attribute?.value)}
+      {words.map((word, wordIndex) => (
+        <div key={`div-${wordIndex}`} className="flex gap-2 place-items-center">
+          <Input
+            type="text"
+            value={word}
+            onChange={(e) => handleInputChange(wordIndex, e.target.value)}
+          />
+          <Button
+            size="sm"
+            className="bg-rose-600 rounded-xl"
+            onClick={() => handleRemoveClick(wordIndex)}
+          >
+            X
+          </Button>
+        </div>
+      ))}
     </div>
   );
 }
