827149c091765076d1d22278e60bd9044b1ca1df
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-06-29 13:37:47 +0530

    delete attribute layout and it values

--- src/components/attributes/AttributeLayout.tsx
+++ src/components/attributes/AttributeLayout.tsx
@@ -22,8 +22,20 @@ function AttributeLayout({
   children,
   onChange,
 }: IAttributeContext) {
+  function handleRemoveClick(): void {
+    if (attribute.name === "data-*") {
+      onChange(index, {});
+    } else {
+      onChange(index, "");
+    }
+    const element = document.getElementById(attribute.name);
+    if (element) {
+      element.remove();
+    }
+  }
+
   return (
-    <div>
+    <div id={attribute.name}>
       <Card className="border-2">
         <CardHeader className="p-3">
           <CardTitle
@@ -32,22 +44,31 @@ function AttributeLayout({
             }
           >
             {attribute.nameForTitile}
-            <HoverCard>
-              <HoverCardTrigger asChild>
-                <Button
-                  variant="outline"
-                  size={"sm"}
-                  className="bg-slate-400 rounded-xl text-sm p-1 w-6 h-6"
-                >
-                  ?
-                </Button>
-              </HoverCardTrigger>
-              <HoverCardContent className="w-64">
-                <div className="">
-                  <p className="text-sm">{attribute.description}</p>
-                </div>
-              </HoverCardContent>
-            </HoverCard>
+            <div className="flex place-items-center gap-1 pr-1">
+              <HoverCard>
+                <HoverCardTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size={"sm"}
+                    className="bg-slate-400 rounded-xl text-xs p-1 w-4 h-4"
+                  >
+                    ?
+                  </Button>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-64">
+                  <div className="">
+                    <p className="text-sm">{attribute.description}</p>
+                  </div>
+                </HoverCardContent>
+              </HoverCard>
+              <Button
+                size="sm"
+                className="bg-rose-600 rounded-xl text-xs p-1 w-4 h-4"
+                onClick={() => handleRemoveClick()}
+              >
+                X
+              </Button>
+            </div>
           </CardTitle>
           {/* <CardDescription>{attribute.description}</CardDescription> */}
         </CardHeader>
