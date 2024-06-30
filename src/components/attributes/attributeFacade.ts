52fac07585ebfe3278d5c55eb82b9e803502ef1a
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-06-30 14:07:51 +0530

    Name for arias

--- src/components/attributes/attributeFacade.ts
+++ src/components/attributes/attributeFacade.ts
@@ -6,7 +6,59 @@ import StringAttribute from "./attributeComponents/StringAttribute";
 import OptionsObjectAttribute from "./attributeComponents/OptionsObjectAttribute";
 import AccessKeyAttribute from "./attributeComponents/AccessKeyAttribute";
 // not add boolean attributes beacuse it only on attribute layout
+
+const ARIA_ATTRIBUTES: { [key: string]: React.ComponentType } = {
+  "aria-activedescendant": StringAttribute,
+  // aria-atomic: boolean,
+  "aria-autocomplete": OptionsAttribute,
+  //"aria-busy": boolean,
+  "aria-checked": OptionsAttribute,
+  "aria-colcount": StringAttribute, // number
+  "aria-colindex": StringAttribute, //number
+  "aria-colspan": StringAttribute, //number
+  "aria-controls": StringAttribute,
+  "aria-current": OptionsAttribute,
+  "aria-describedby": StringAttribute,
+  "aria-details": StringAttribute,
+  //"aria-disabled": boolean,
+  "aria-dropeffect": OptionsAttribute,
+  "aria-errormessage": StringAttribute,
+  //"aria-expanded": boolean,
+  "aria-flowto": StringAttribute,
+  //"aria-grabbed": boolean,
+  "aria-haspopup": OptionsAttribute,
+  //"aria-hidden": boolean,
+  "aria-invalid": OptionsAttribute,
+  "aria-keyshortcuts": StringAttribute,
+  "aria-label": StringAttribute,
+  "aria-labelledby": StringAttribute,
+  "aria-level": StringAttribute, //number
+  "aria-live": OptionsAttribute,
+  //"aria-modal": boolean,
+  //"aria-multiline": boolean,
+  //"aria-multiselectable": boolean,
+  "aria-orientation": OptionsAttribute,
+  "aria-owns": StringAttribute,
+  "aria-placeholder": StringAttribute,
+  "aria-posinset": StringAttribute, //number
+  "aria-pressed": OptionsAttribute,
+  //"aria-readonly": boolean,
+  "aria-relevant": OptionsAttribute,
+  //"aria-required": boolean,
+  "aria-roledescription": StringAttribute,
+  "aria-rowcount": StringAttribute, //number
+  "aria-rowindex": StringAttribute, //number
+  "aria-rowspan": StringAttribute, //number
+  //"aria-selected": boolean,
+  "aria-setsize": StringAttribute, //number
+  "aria-sort": OptionsAttribute,
+  "aria-valuemax": StringAttribute, //number
+  "aria-valuemin": StringAttribute, //number
+  "aria-valuenow": StringAttribute, //number
+  "aria-valuetext": StringAttribute,
+};
 export const attributeComponents: { [key: string]: React.ComponentType } = {
+  ...ARIA_ATTRIBUTES,
   accesskey: AccessKeyAttribute,
   autocapitalize: OptionsAttribute,
   class: ClassAttribute,
--- src/utils/attributes/ariaAttributes.ts
+++ src/utils/attributes/ariaAttributes.ts
@@ -3,6 +3,7 @@ import { Attribute } from "../../types/attributeTypes";
 export const ARIA_ATTRIBUTES: Attribute[] = [
   {
     name: "aria-activedescendant",
+    nameForTitile: "ARIA - ActiveDescendant",
     type: "string",
     description:
       "Identifies the currently active element when focus is on a composite widget, textbox, group, or application.",
@@ -10,6 +11,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-atomic",
+    nameForTitile: "ARIA - Atomic",
     type: "boolean",
     description:
       "Indicates whether assistive technologies will present all, or only parts of, the changed region based on the change notifications defined by the aria-relevant attribute.",
@@ -17,6 +19,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-autocomplete",
+    nameForTitile: "ARIA - AutoComplete",
     type: "string",
     description:
       "Indicates whether user input completion suggestions are provided.",
@@ -25,6 +28,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-busy",
+    nameForTitile: "ARIA - Busy",
     type: "boolean",
     description:
       "Indicates whether an element, and its subtree, are currently being updated.",
@@ -32,6 +36,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-checked",
+    nameForTitile: "ARIA - Checked",
     type: "boolean",
     description:
       'Indicates the current "checked" state of checkboxes, radio buttons, and other widgets.',
@@ -40,19 +45,22 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-colcount",
+    nameForTitile: "ARIA - ColCount",
     type: "number",
     description: "Defines the number of columns in a table, grid, or treegrid.",
     value: "",
   },
   {
     name: "aria-colindex",
+    nameForTitile: "ARIA - ColIndex",
     type: "number",
     description:
-      "Defines an element’s column index or position with respect to the total number of columns within a table, grid, or treegrid.",
+      "Defines an elements column index or position with respect to the total number of columns within a table, grid, or treegrid.",
     value: "",
   },
   {
     name: "aria-colspan",
+    nameForTitile: "ARIA - ColSpan",
     type: "number",
     description:
       "Defines the number of columns spanned by a cell or gridcell within a table, grid, or treegrid.",
@@ -60,6 +68,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-controls",
+    nameForTitile: "ARIA - Controls",
     type: "string",
     description:
       "Identifies the element (or elements) whose contents or presence are controlled by the current element.",
@@ -67,6 +76,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-current",
+    nameForTitile: "ARIA - Current",
     type: "string",
     description:
       "Indicates the element that represents the current item within a container or set of related elements.",
@@ -75,6 +85,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-describedby",
+    nameForTitile: "ARIA - DescribedBy",
     type: "string",
     description:
       "Identifies the element (or elements) that describes the object.",
@@ -82,6 +93,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-details",
+    nameForTitile: "ARIA - Details",
     type: "string",
     description:
       "Identifies the element that provides a detailed, extended description for the object.",
@@ -89,6 +101,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-disabled",
+    nameForTitile: "ARIA - Disabled",
     type: "boolean",
     description:
       "Indicates that the element is perceivable but disabled, so it is not editable or otherwise operable.",
@@ -96,6 +109,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-dropeffect",
+    nameForTitile: "ARIA - DropeEffect",
     type: "string",
     description:
       "Indicates what functions can be performed when the dragged object is released on the drop target.",
@@ -104,6 +118,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-errormessage",
+    nameForTitile: "ARIA - ErrorMessage",
     type: "string",
     description:
       "Identifies the element that provides an error message for the object.",
@@ -111,6 +126,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-expanded",
+    nameForTitile: "ARIA - Expanded",
     type: "boolean",
     description:
       "Indicates whether the element, or another grouping element it controls, is currently expanded or collapsed.",
@@ -118,6 +134,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-flowto",
+    nameForTitile: "ARIA - FlowTo",
     type: "string",
     description:
       "Identifies the next element (or elements) in an alternate reading order of content which, at the user’s discretion, allows assistive technology to override the general default reading order.",
@@ -125,13 +142,15 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-grabbed",
+    nameForTitile: "ARIA - Grabbed",
     type: "boolean",
     description:
-      'Indicates an element’s "grabbed" state in a drag-and-drop operation.',
+      'Indicates an elements "grabbed" state in a drag-and-drop operation.',
     value: "",
   },
   {
     name: "aria-haspopup",
+    nameForTitile: "ARIA - HasPopup",
     type: "string",
     description:
       "Indicates the availability and type of interactive popup element, such as menu or dialog, that can be triggered by an element.",
@@ -140,6 +159,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-hidden",
+    nameForTitile: "ARIA - Hidden",
     type: "boolean",
     description:
       "Indicates whether the element is exposed to an accessibility API.",
@@ -147,6 +167,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-invalid",
+    nameForTitile: "ARIA - Invalid",
     type: "string",
     description:
       "Indicates the entered value does not conform to the format expected by the application.",
@@ -155,6 +176,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-keyshortcuts",
+    nameForTitile: "ARIA - KeyShortcuts",
     type: "string",
     description:
       "Indicates keyboard shortcuts that an author has implemented to activate or give focus to an element.",
@@ -162,12 +184,14 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-label",
+    nameForTitile: "Aria - Label",
     type: "string",
     description: "Defines a string value that labels the current element.",
     value: "",
   },
   {
     name: "aria-labelledby",
+    nameForTitile: "ARIA - LabelledBy",
     type: "string",
     description:
       "Identifies the element (or elements) that labels the current element.",
@@ -175,6 +199,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-level",
+    nameForTitile: "ARIA - Level",
     type: "number",
     description:
       "Defines the hierarchical level of an element within a structure.",
@@ -182,6 +207,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-live",
+    nameForTitile: "ARIA - Live",
     type: "string",
     description:
       "Indicates that an element will be updated, and describes the types of updates the user agents, assistive technologies, and user can expect from the live region.",
@@ -190,12 +216,14 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-modal",
+    nameForTitile: "ARIA - Modal",
     type: "boolean",
     description: "Indicates whether an element is modal when displayed.",
     value: "",
   },
   {
     name: "aria-multiline",
+    nameForTitile: "ARIA - MultiLine",
     type: "boolean",
     description:
       "Indicates whether a text box accepts multiple lines of input or only a single line.",
@@ -203,6 +231,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-multiselectable",
+    nameForTitile: "ARIA - MultiSelectable",
     type: "boolean",
     description:
       "Indicates that the user may select more than one item from the current selectable descendants.",
@@ -210,14 +239,16 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-orientation",
+    nameForTitile: "ARIA - Orientation",
     type: "string",
     description:
-      "Indicates whether the element’s orientation is horizontal, vertical, or unknown/ambiguous.",
+      "Indicates whether the elements orientation is horizontal, vertical, or unknown/ambiguous.",
     value: "",
     options: ["horizontal", "vertical"],
   },
   {
     name: "aria-owns",
+    nameForTitile: "ARIA - Owns",
     type: "string",
     description:
       "Identifies an element (or elements) in order to define a relationship between that element and the current element.",
@@ -225,6 +256,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-placeholder",
+    nameForTitile: "ARIA - PlaceHolder",
     type: "string",
     description:
       "Defines a short hint (a word or short phrase) intended to aid the user with data entry when the control has no value.",
@@ -232,13 +264,15 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-posinset",
+    nameForTitile: "ARIA - Posinset",
     type: "number",
     description:
-      "Defines an element’s number or position in the current set of listitems or treeitems.",
+      "Defines an elements number or position in the current set of listitems or treeitems.",
     value: "",
   },
   {
     name: "aria-pressed",
+    nameForTitile: "ARIA - Pressed",
     type: "boolean",
     description: 'Indicates the current "pressed" state of toggle buttons.',
     value: "",
@@ -246,6 +280,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-readonly",
+    nameForTitile: "ARIA - Readonly",
     type: "boolean",
     description:
       "Indicates that the element is not editable, but is otherwise operable.",
@@ -253,6 +288,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-relevant",
+    nameForTitile: "ARIA - Relevant",
     type: "string",
     description:
       "Indicates what notifications the user agent will trigger when the accessibility tree within a live region is modified.",
@@ -261,6 +297,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-required",
+    nameForTitile: "ARIA - Required",
     type: "boolean",
     description:
       "Indicates that user input is required on the element before a form may be submitted.",
@@ -268,6 +305,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-roledescription",
+    nameForTitile: "ARIA - RoleDescription",
     type: "string",
     description:
       "Defines a human-readable, author-localized description for the role of an element.",
@@ -275,19 +313,22 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-rowcount",
+    nameForTitile: "ARIA - RowCount",
     type: "number",
     description: "Defines the number of rows in a table, grid, or treegrid.",
     value: "",
   },
   {
     name: "aria-rowindex",
+    nameForTitile: "ARIA - RowIndex",
     type: "number",
     description:
-      "Defines an element’s row index or position with respect to the total number of rows within a table, grid, or treegrid.",
+      "Defines an elements row index or position with respect to the total number of rows within a table, grid, or treegrid.",
     value: "",
   },
   {
     name: "aria-rowspan",
+    nameForTitile: "ARIA - RowSpan",
     type: "number",
     description:
       "Defines the number of rows spanned by a cell or gridcell within a table, grid, or treegrid.",
@@ -295,12 +336,14 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-selected",
+    nameForTitile: "ARIA - Selected",
     type: "boolean",
     description: 'Indicates the current "selected" state of various widgets.',
     value: "",
   },
   {
     name: "aria-setsize",
+    nameForTitile: "ARIA - SetSize",
     type: "number",
     description:
       "Defines the number of items in the current set of listitems or treeitems.",
@@ -308,6 +351,7 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-sort",
+    nameForTitile: "ARIA - Sort",
     type: "string",
     description:
       "Indicates if items in a table or grid are sorted in ascending or descending order.",
@@ -316,24 +360,28 @@ export const ARIA_ATTRIBUTES: Attribute[] = [
   },
   {
     name: "aria-valuemax",
+    nameForTitile: "ARIA - ValueMax",
     type: "number",
     description: "Defines the maximum allowed value for a range widget.",
     value: "",
   },
   {
     name: "aria-valuemin",
+    nameForTitile: "ARIA - ValueMin",
     type: "number",
     description: "Defines the minimum allowed value for a range widget.",
     value: "",
   },
   {
     name: "aria-valuenow",
+    nameForTitile: "ARIA - ValueNow",
     type: "number",
     description: "Defines the current value for a range widget.",
     value: "",
   },
   {
     name: "aria-valuetext",
+    nameForTitile: "ARIA - ValueText",
     type: "string",
     description:
       "Defines the human-readable text alternative of aria-valuenow for a range widget.",
