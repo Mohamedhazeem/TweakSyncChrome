ceeb09def1afe7b00b7fb2cdaeaf2b7c6a28472a
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-07-13 11:26:56 +0530

    fix add style props bug

--- src/components/styles/styleHelperComponents/AddStyleProperty.tsx
+++ src/components/styles/styleHelperComponents/AddStyleProperty.tsx
@@ -10,11 +10,7 @@ import {
   CommandList,
   CommandSeparator,
 } from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { capitalizeFirstLetter } from "@/utils/capitalizeFirstLetter";
 import { GLOBAL_STYLES } from "@/utils/styles/styles";
 import { ElementStyles } from "@/types/elementTypes";
@@ -25,59 +21,45 @@ interface AddStylePropertyProps {
   addStyleProperty: (selector: string, property: string) => void;
 }
 
-function AddStyleProperty({
-  selector,
-  setStyles,
-  addStyleProperty,
-}: AddStylePropertyProps) {
+function AddStyleProperty({ selector, setStyles, addStyleProperty }: AddStylePropertyProps) {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState<string>();
 
-  const handleSelect = (newValue: string) => {
-    setValue(newValue === value ? "" : newValue);
+  const handleSelect = (property: string) => {
+    setValue(property === value ? "" : property);
     setStyles((prevStyles) => {
-      const newStyleProperty = GLOBAL_STYLES.find(
-        (style) => style.name === newValue
-      );
+      const newStyleProperty = GLOBAL_STYLES.find((style) => style.name === property);
 
-      if (newStyleProperty) {
+      if (!newStyleProperty) {
         const updatedStyles = { ...prevStyles };
 
         if (selector === "inline") {
-          updatedStyles.inline[newStyleProperty.name] = "";
+          updatedStyles.inline[property] = "";
         } else if (updatedStyles.external.classes[selector]) {
-          updatedStyles.external.classes[selector][newStyleProperty.name] = "";
+          updatedStyles.external.classes[selector][property] = "";
         } else if (updatedStyles.external.ids[selector]) {
-          updatedStyles.external.ids[selector][newStyleProperty.name] = "";
+          updatedStyles.external.ids[selector][property] = "";
         } else if (updatedStyles.external.tags[selector]) {
-          updatedStyles.external.tags[selector][newStyleProperty.name] = "";
+          updatedStyles.external.tags[selector][property] = "";
         } else if (updatedStyles.external.attribute[selector]) {
-          updatedStyles.external.attribute[selector][newStyleProperty.name] =
-            "";
+          updatedStyles.external.attribute[selector][property] = "";
         } else if (updatedStyles.external.descendant[selector]) {
-          updatedStyles.external.descendant[selector][newStyleProperty.name] =
-            "";
+          updatedStyles.external.descendant[selector][property] = "";
         } else if (updatedStyles.external.pseudoElementStyles[selector]) {
-          updatedStyles.external.pseudoElementStyles[selector][
-            newStyleProperty.name
-          ] = "";
+          updatedStyles.external.pseudoElementStyles[selector][property] = "";
         } else if (updatedStyles.external.pseudoClassStyles[selector]) {
-          updatedStyles.external.pseudoClassStyles[selector][
-            newStyleProperty.name
-          ] = "";
+          updatedStyles.external.pseudoClassStyles[selector][property] = "";
         } else if (updatedStyles.external.atRules[selector]) {
           for (const subSelector in updatedStyles.external.atRules[selector]) {
-            updatedStyles.external.atRules[selector][subSelector][
-              newStyleProperty.name
-            ] = "";
+            updatedStyles.external.atRules[selector][subSelector][property] = "";
           }
         } else {
           updatedStyles.external.classes[selector] = {
-            [newStyleProperty.name]: "",
+            [property]: "",
           };
         }
 
-        addStyleProperty(selector, newStyleProperty.name);
+        addStyleProperty(selector, property);
         return updatedStyles;
       }
 
@@ -90,12 +72,7 @@ function AddStyleProperty({
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-full"
-        >
+        <Button variant="outline" role="combobox" aria-expanded={open} className="w-full">
           Add Style Property
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
@@ -125,9 +102,7 @@ function AddStyleProperty({
               ))}
             </CommandGroup>
 
-            {GLOBAL_STYLES.length === 0 && (
-              <CommandEmpty>No style properties found.</CommandEmpty>
-            )}
+            {GLOBAL_STYLES.length === 0 && <CommandEmpty>No style properties found.</CommandEmpty>}
           </CommandList>
         </Command>
       </PopoverContent>
