72165ac61ea460b59f91ed0fc79bfaa1e6a5d1f9
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-07-21 11:28:59 +0530

    update multi option ui

--- src/components/styles/styleHelperComponents/MultiStyleOptions.tsx
+++ src/components/styles/styleHelperComponents/MultiStyleOptions.tsx
@@ -13,6 +13,7 @@ import { capitalizeFirstLetter } from "@/utils/capitalizeFirstLetter";
 import { splitStringToArray } from "@/utils/splitStringToArray";
 import { Style } from "@/types/styleTypes";
 import { ChevronsUpDown } from "lucide-react";
+// import BackgroundPosition from "../styleComponents/BackgroundPosition";
 interface MultiOptionsStyleProps {
   style: Style;
   customOptionsCallback: (newValue: string | boolean) => void;
@@ -64,55 +65,58 @@ const MultiStyleOptions: React.FC<MultiOptionsStyleProps> = ({ style, customOpti
   return (
     <div className="flex flex-col gap-2">
       {selectedOptions.map((option, index) => (
-        <div key={`option-${index}`} className="flex gap-2 items-center">
-          <Popover
-            open={openPopoverIndex === index}
-            onOpenChange={(newOpenState) => setOpenPopoverIndex(newOpenState ? index : null)}
-          >
-            <PopoverTrigger asChild>
-              <Button variant="outline" size="default" className="w-full justify-between">
-                {option ? capitalizeFirstLetter(option) : `Select ${nameForTitle}`}
-                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-              </Button>
-            </PopoverTrigger>
-            {openPopoverIndex === index && (
-              <PopoverContent className="w-[200px] p-0">
-                <Command>
-                  <CommandInput placeholder={`Search Options...`} />
-                  <CommandList>
-                    <CommandEmpty>No option found.</CommandEmpty>
-                    <CommandGroup>
-                      {Array.isArray(options) &&
-                        options!.map((opt) => (
-                          <CommandItem
-                            key={opt}
-                            value={opt}
-                            onSelect={() => handleSelect(index, opt)}
-                          >
-                            {capitalizeFirstLetter(opt)}
-                          </CommandItem>
-                        ))}
-                    </CommandGroup>
-                  </CommandList>
-                </Command>
-              </PopoverContent>
-            )}
-          </Popover>
-          <Button
-            size="sm"
-            className="bg-rose-600 rounded-xl text-xs p-1 w-4 h-4"
-            onClick={() => handleRemoveClick(index)}
-          >
-            X
-          </Button>
+        <div className="flex flex-col gap-2 items-center">
+          <div key={`option-${index}`} className="flex gap-2 items-center w-full">
+            <Popover
+              open={openPopoverIndex === index}
+              onOpenChange={(newOpenState) => setOpenPopoverIndex(newOpenState ? index : null)}
+            >
+              <PopoverTrigger asChild>
+                <Button variant="outline" size="default" className="w-full justify-between h-7">
+                  {option ? capitalizeFirstLetter(option) : `Select ${nameForTitle}`}
+                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              {openPopoverIndex === index && (
+                <PopoverContent className="w-[200px] p-0">
+                  <Command>
+                    <CommandInput placeholder={`Search Options...`} />
+                    <CommandList>
+                      <CommandEmpty>No option found.</CommandEmpty>
+                      <CommandGroup>
+                        {Array.isArray(options) &&
+                          options!.map((opt) => (
+                            <CommandItem
+                              key={opt}
+                              value={opt}
+                              onSelect={() => handleSelect(index, opt)}
+                            >
+                              {capitalizeFirstLetter(opt)}
+                            </CommandItem>
+                          ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              )}
+            </Popover>
+            <Button
+              size="sm"
+              className="bg-rose-600 rounded-xl text-xs p-1 w-4 h-4"
+              onClick={() => handleRemoveClick(index)}
+            >
+              X
+            </Button>
+          </div>
         </div>
       ))}
       <Button
-        size="default"
+        size="sm"
         onClick={handleAddOption}
-        className="w-full self-center"
+        className="h-7 self-center"
         disabled={maxOptionCounts ? optionCount >= maxOptionCounts : false}
       >
+        {/* Add */}
         Add {nameForTitle}
       </Button>
     </div>
