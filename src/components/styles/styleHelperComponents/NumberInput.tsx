62182f35014ac6bb8ca18d33283d3b8308d720de
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-07-24 13:31:12 +0530

    Number Input

---
+++ src/components/styles/styleHelperComponents/NumberInput.tsx
@@ -0,0 +1,20 @@
+import { Input } from "@/components/ui/input";
+
+type NumberInputType = {
+  number: string;
+  setNumber: React.Dispatch<React.SetStateAction<string>>;
+  customOptionsCallback: (newValue: string) => void;
+  sign?: string;
+};
+export function NumberInput({ number, setNumber, customOptionsCallback, sign }: NumberInputType) {
+  return (
+    <Input
+      type="number"
+      value={number}
+      onChange={(e) => {
+        setNumber(e.target.value);
+        customOptionsCallback(`${e.target.value}${sign}`);
+      }}
+    />
+  );
+}
