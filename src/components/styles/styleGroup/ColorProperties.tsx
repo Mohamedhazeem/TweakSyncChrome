2f52b7c9e5362f1cfe8b4450f98f75ef1defb88b
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-07-31 11:35:21 +0530

    accent-color

--- src/components/styles/styleGroup/ColorProperties.tsx
+++ src/components/styles/styleGroup/ColorProperties.tsx
@@ -4,6 +4,7 @@ import SingleStyleOptionSetter from "../styleComponents/SingleStyleOptionSetter"
 function ColorProperties() {
   return (
     <div className="inspector-component">
+      <Color colorProp="accent-color" />
       <Color colorProp="color" />
       <SingleStyleOptionSetter name="color-scheme" isCapitalized={true} />
     </div>
