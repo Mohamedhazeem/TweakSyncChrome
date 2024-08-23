f1cf2ed75eee874f92be758e3bf989534d741ebc
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-08-23 12:08:06 +0530

    options grouped

--- src/components/styles/styleHelperComponents/PopOver.tsx
+++ src/components/styles/styleHelperComponents/PopOver.tsx
@@ -8,6 +8,7 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
+  CommandSeparator,
 } from "@/components/ui/command";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Style } from "@/types/styleTypes";
@@ -15,6 +16,7 @@ import { capitalizeFirstLetter } from "@/utils/capitalizeFirstLetter";
 import { getButtonText } from "@/utils/styles/getButtonTextForPopver";
 import { useClearLayoutContext } from "@/utils/elementContext";
 import { sortOptions } from "@/utils/sortOptions";
+import { globalCssOptions } from "@/utils/styles/globalStyles";
 
 type PopOverType = {
   open: boolean;
@@ -37,6 +39,12 @@ export function PopOver({
   option,
 }: PopOverType) {
   const clearLayout = useClearLayoutContext();
+  const globalCss = (Array.isArray(style?.options) ? style.options : []).filter((opt) =>
+    globalCssOptions.includes(opt)
+  );
+  const specificCss = (Array.isArray(style?.options) ? style.options : []).filter(
+    (opt) => !globalCssOptions.includes(opt)
+  );
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -58,30 +66,49 @@ export function PopOver({
           <CommandInput placeholder="Search Options..." />
           <CommandList>
             <CommandEmpty>No option found.</CommandEmpty>
-            <CommandGroup>
-              {style &&
-                style.options &&
-                Array.isArray(style.options) &&
-                sortOptions(style?.options).map((ops) => (
-                  <CommandItem
-                    key={ops}
-                    value={ops}
-                    onSelect={(currentValue) => handleSelect(currentValue)}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        (ops === "custom" && isCustomValue) || ops === option
-                          ? clearLayout
-                            ? "opacity-0"
-                            : "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
+            <CommandGroup heading={specificCss && `${style?.nameForTitle} Options`}>
+              {sortOptions(specificCss).map((ops) => (
+                <CommandItem
+                  key={ops}
+                  value={ops}
+                  onSelect={(currentValue) => handleSelect(currentValue)}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      (ops === "custom" && isCustomValue) || ops === option
+                        ? clearLayout
+                          ? "opacity-0"
+                          : "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                  {isCaptilized ? capitalizeFirstLetter(ops) : ops}
+                </CommandItem>
+              ))}
+            </CommandGroup>
 
-                    {isCaptilized ? capitalizeFirstLetter(ops) : ops}
-                  </CommandItem>
-                ))}
+            <CommandSeparator />
+            <CommandGroup heading="Global CSS">
+              {sortOptions(globalCss).map((ops) => (
+                <CommandItem
+                  key={ops}
+                  value={ops}
+                  onSelect={(currentValue) => handleSelect(currentValue)}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      (ops === "custom" && isCustomValue) || ops === option
+                        ? clearLayout
+                          ? "opacity-0"
+                          : "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                  {isCaptilized ? capitalizeFirstLetter(ops) : ops}
+                </CommandItem>
+              ))}
             </CommandGroup>
           </CommandList>
         </Command>
