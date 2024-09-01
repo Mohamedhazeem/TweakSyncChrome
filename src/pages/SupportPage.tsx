96d7de1468fd9094fc794d9f51e437788996e2b8
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-09-01 12:17:42 +0530

    Support Page Added

---
+++ src/pages/SupportPage.tsx
@@ -0,0 +1,55 @@
+import { FacebookIcon } from "@/components/Icons/socialMediaIcons/FacebookIcon";
+import { ContactSupportIcon } from "../components/Icons/ContactSupportIcon";
+import { InstagramIcon } from "@/components/Icons/socialMediaIcons/InstagramIcon";
+import { RedditIcon } from "@/components/Icons/socialMediaIcons/RedditIcon";
+import { TwitterIcon } from "@/components/Icons/socialMediaIcons/TwitterIcon";
+
+function SupportPage() {
+  return (
+    <div className="bg-gradient-to-r from-indigo-500 to-red-400 h-dvh">
+      <div className="SupportContainer">
+        <div className="contactSupport">
+          <p className="contactSupportTitle">Contact Support</p>
+          <ContactSupportIcon />
+          <p className="contactSupportDialogue">Still have a question or need our help?</p>
+          <p className="contactSupportDialogue">
+            Is there a feature you'd like to see? Let us know!
+          </p>
+          <a className="contactMail" href="mailto:insteadbusiness@gmail.com">
+            insteadbusiness@gmail.com
+          </a>
+          <p className="socialLinkDialogue">You can also find us here:</p>
+          <div className="socialMediaIcons">
+            {/* need to update linke */}
+            <a
+              href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F"
+              title="Facebook"
+            >
+              <FacebookIcon />
+            </a>
+            <a
+              href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F"
+              title="Instagram"
+            >
+              <InstagramIcon />
+            </a>
+            <a
+              href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F"
+              title="Reddit"
+            >
+              <RedditIcon />
+            </a>
+            <a
+              href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F"
+              title="X"
+            >
+              <TwitterIcon />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SupportPage;
